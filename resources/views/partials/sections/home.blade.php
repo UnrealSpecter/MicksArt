58a2ef11492b4ce4403fa60b6a7bd6170516d9f1
Tomi <tomi_emmen@hotmail.com>
2018-07-24 21:36:09 +0000

    micksart

--- resources/views/partials/sections/home.blade.php
+++ resources/views/partials/sections/home.blade.php
@@ -1,5 +1,4 @@
 <div class="section-wrapper d-flex flex-column justify-content-center align-items-center">
-    <embed src="assets/home.svg" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;">
-    <embed class="col-3" src="{{ asset('/assets/logo.svg')}}">
+    <img src="{{ asset('/assets/logo.svg')}}" class="col-3">
     <h1 style="z-index: 2;">micksart</h1>
 </div>
