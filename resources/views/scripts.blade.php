4f4367b705b70adec0ad7401aa2092b5b36f3474
vagrant <vagrant@homestead>
2018-08-01 11:02:20 +0000

    capitalization error fix on live

--- resources/views/scripts.blade.php
+++ resources/views/scripts.blade.php
@@ -5,6 +5,6 @@
     <script src="{{ URL::asset('plugins/hammer/js/hammer.min.js') }}"></script>
     <script src="{{ URL::asset('plugins/hammer/js/jquery-hammer.js') }}"></script>
     <script src="{{ URL::asset('plugins/bootstrap-4.0/js/bootstrap.min.js')}}"></script>
-    <script src="{{ URL::asset('plugins/fullpage-scroll/js/jquery.fullpage.js')}}"></script>
+    <script src="{{ URL::asset('plugins/fullpage-scroll/js/fullpage.js')}}"></script>
 
     @yield('additional-scripts')
