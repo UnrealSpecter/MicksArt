06b0808d2ff26baf360ac8b47a9aede72dadb663
vagrant <vagrant@homestead>
2019-05-20 11:22:00 +0000

    fixed logo responsiveness

--- resources/views/partials/sections/home.blade.php
+++ resources/views/partials/sections/home.blade.php
@@ -1,4 +1,4 @@
-<div class="section-wrapper d-flex flex-column justify-content-center align-items-center">
+<div class="section-wrapper d-flex flex-row justify-content-center align-items-center">
     <img class="background desktop" src="{{ asset('/assets/png/home.png')}}">
     <img class="background mobile" src="{{ asset('/assets/png/mobile-home.png')}}">
     <img src="{{ asset('/assets/png/logo-kunstuitleen.png')}}" class="col-6 col-sm-3 col-md-5 col-lg-3 col-xl-3 no-padding-margin animated fadeIn">
